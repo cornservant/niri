a33d768545f9d3919e0da0a21f4ebc69842da12c
Jakob Hellermann <jakob.hellermann@protonmail.com>
2026-04-28 12:22:35 +0200

    window-rule: Add is-fullscreen matcher

--- niri-config/src/lib.rs
+++ niri-config/src/lib.rs
@@ -899,6 +899,7 @@ mod tests {
                 match app-id=".*alacritty"
                 exclude title="~"
                 exclude is-active=true is-focused=false
+                exclude is-fullscreen=true
 
                 open-on-output "eDP-1"
                 open-maximized true
@@ -1762,6 +1763,21 @@ mod tests {
                             is_floating: None,
                             is_window_cast_target: None,
                             is_urgent: None,
+                            is_fullscreen: None,
+                            at_startup: None,
+                        },
+                        Match {
+                            app_id: None,
+                            title: None,
+                            is_active: None,
+                            is_focused: None,
+                            is_active_in_column: None,
+                            is_floating: None,
+                            is_window_cast_target: None,
+                            is_urgent: None,
+                            is_fullscreen: Some(
+                                true,
+                            ),
                             at_startup: None,
                         },
                     ],
@@ -1781,6 +1797,7 @@ mod tests {
                             is_floating: None,
                             is_window_cast_target: None,
                             is_urgent: None,
+                            is_fullscreen: None,
                             at_startup: None,
                         },
                         Match {
@@ -1796,6 +1813,7 @@ mod tests {
                             is_floating: None,
                             is_window_cast_target: None,
                             is_urgent: None,
+                            is_fullscreen: None,
                             at_startup: None,
                         },
                     ],
