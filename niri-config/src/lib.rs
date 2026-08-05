958b60b6be83730d56c1831f8282c41c2e1dae3a
MithicSpirit <rpc01234@gmail.com>
2026-08-04 23:19:59 -0400

    feat: force render
    Renders a window when it is being screencast or as configured by the
    force-render window rule.  Maximum frame rate of windows being rendered
    this way is controlled by the force-render-fps window rule (unlimited by
    default, or when explicitly set to 0).

--- niri-config/src/lib.rs
+++ niri-config/src/lib.rs
@@ -1882,6 +1882,8 @@ mod tests {
                     ),
                     scroll_factor: None,
                     tiled_state: None,
+                    force_render: None,
+                    force_render_fps: None,
                     background_effect: BackgroundEffectRule {
                         xray: None,
                         blur: None,
